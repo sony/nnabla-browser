4d49a780384fc9dbd084ce693b235ea615696645
s-takuseno <Takuma.Seno@sony.com>
2020-11-05 16:17:33 +0900

    Fix python requirement version

--- setup.py
+++ setup.py
@@ -1,35 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nnablabrowser",
+    name="nnabla-browser",
     version="0.1.0b",
     description="A browser base visualizer for Neural Network Libraries (nnabla).",
-    # url=...,
+    url='https://github.com/sony/nnabla-browser',
     author="Akio Hayakawa",
     # license=...,
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Neural Network framework :: Visualization Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        # todo:  'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # todo: check python version compatibility
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
@@ -42,8 +23,7 @@ setup(
         "numpy",
         "nnabla"
     ],
-    # python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <=3.6.5",
-    python_requires=">=3.5, <=3.6.5",
+    python_requires=">=3.5",
     package_dir={"nnabla_browser": "nnabla_browser"},
     packages=["nnabla_browser"],
     package_data={'nnabla_browser': ["nnabla_core/*"]}, # TODO: include built JS
