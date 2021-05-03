679659da03b33d61daa4b165affd536432a20788
AkioHayakawa <Akio.Hayakawa@sony.com>
2021-05-03 19:38:48 +0900

    Delete snapshot

--- front/src/components/center/GraphViewer.vue
+++ front/src/components/center/GraphViewer.vue
@@ -1,28 +1,6 @@
 <template>
   <div>
     <graph-tab-list @history="command => $emit('history', command)" />
-    <div
-      v-if="activeFileTag"
-      class="tool-icon-container"
-      @click="capSwitch"
-    >
-      <font-awesome-icon
-        v-show="!snapshotLoding"
-        icon="camera"
-        class="func-icon"
-      />
-      <font-awesome-icon
-        v-show="snapshotLoding"
-        icon="spinner"
-        class="fa-spin func-icon"
-      />
-      <snap-shot
-        :cap-element="snapshotSwitch"
-        container-id="network-container"
-        :image-name="activeFileTag"
-        @snapshot-finish="snapshotLoding = !$event"
-      />
-    </div>
     <div class="tab-content network-editor-scroller">
       <svg-area />
     </div>
@@ -30,59 +8,17 @@
 </template>
 
 <script lang="ts">
-import { faCamera, faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { Definitions } from '@/utils/definitions'
 import GraphTabList from '@/components/center/graph/GraphTabList.vue'
 import SvgArea from '@/components/center/graph/SvgArea.vue'
 import Vue from 'vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import snapShot from '@/components/tools/SnapShot.vue'
 
-library.add(faSpinner, faCamera)
-
-interface DataType {
-  layerTextClipId: string;
-  layerTextClipWidth: number;
-  layerTextClipHeight: number;
-  snapshotSwitch: boolean | undefined;
-  snapshotLoding: boolean | undefined;
-}
-
-export default Vue.extend<DataType, {}, {}, {}>({
+export default Vue.extend({
   components: {
     'graph-tab-list': GraphTabList,
-    'svg-area': SvgArea,
-    'snap-shot': snapShot
+    'svg-area': SvgArea
   },
   props: {
     historyInfo: Object
-  },
-  data: function () {
-    return {
-      layerTextClipId: Definitions.EDIT.LAYER.CLIP_PATH.ID,
-      layerTextClipWidth: Definitions.EDIT.LAYER.CLIP_PATH.WIDTH,
-      layerTextClipHeight: Definitions.EDIT.LAYER.CLIP_PATH.HEIGHT,
-      snapshotSwitch: undefined,
-      snapshotLoding: undefined
-    }
-  },
-  computed: {
-    activeFileTag: function () {
-      const tag = this.$store.state.directoryInfo.activeFile.replace(
-        /[\\/.]/g,
-        '-'
-      )
-      return tag.replace(/^-*/, '')
-    }
-  },
-  methods: {
-    capSwitch () {
-      if (this.snapshotLoding) {
-        return
-      }
-      this.snapshotLoding = true
-      this.snapshotSwitch = !this.snapshotSwitch
-    }
   }
 })
 </script>
